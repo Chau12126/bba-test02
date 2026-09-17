4d6cc6309b47c3cdaa0f70e0c1355130f54eb0ab
test02-Nguyen Chau <=>
2026-09-17 12:32:43 +0700

    feat: add solution for calm belt challenge

---
+++ tests/lesson-04/calm-belt.js
@@ -0,0 +1,14 @@
+let navigatorName = 'Luffy';
+let currentHakiLevel = 3;
+
+const islandA = 15;
+const islandB = 25;
+const islandC = 40;
+
+const sum = islandA + islandB +islandC
+const average = sum/3
+console.log (`Tổng lượng thịt tiêu thụ: ${sum}` );
+console.log (`Trung bình lượng thịt tiêu thụ: ${average}` );
+
+const residual = sum % 3;
+console.log(`Số thịt còn dư khi đã chia đều: ${residual}`);
