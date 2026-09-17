31351712da8511d791388c0dea64f2326b93f00a
test02-Nguyen Chau <=>
2026-09-17 13:22:07 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/calm-belt-2.js
@@ -0,0 +1,14 @@
+let develFruit = 'gomu-gomu';
+let effect = "";
+if (develFruit === "gomu-gomu") {
+    effect = 'Gear 5 Unlocked!';
+} else if (develFruit === "mera-mera") {
+    effect = 'Flame Emperor!';
+} else if (develFruit === "ope-ope") {
+    effect = 'Room & Ope!';
+} else if (develFruit === "none") {
+    effect = 'Normal Human';
+} else {
+    effect = 'Unknown Devil Fruit';
+}
+console.log(effect);
